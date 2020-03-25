f159bc64d7878f5ef34871b14c4f44d4a1936cf7
gavinoconnor <gedmund@gmail.com>
2020-03-25 13:36:49 -0400

    add logic for users and application controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_many :user_habits
+  has_many :habits, through: :user_habits
+  has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: {minimum: 3}
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
